9cdf56b1b991138831aa42abfa484928731733aa
Aland <aland.zland@gmail.com>
2025-06-05 18:50:48 +0300

    0000

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('/privacy', (req, res) => {
 // Sync Sequelize & Start Server 
 sequelize.sync({alter: true}) // {force: true}
     .then(() => {
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`Server is running on port ${PORT}`);
         });
     })
